4894cbc91223ef9f8929e6289b51d88833cc0e16
havoceternal <ambiiswet@gmail.com>
2025-08-14 22:03:46 +1000

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -1404,182 +1404,191 @@ do
     end;
 
     function Funcs:AddButton(...)
-    local Button = {}
-
-    -- Handles parameters for both full and sub buttons
-    local function ProcessButtonParams(Class, Obj, ...)
-        local Props = select(1, ...)
-        if type(Props) == 'table' then
-            Obj.Text = Props.Text
-            Obj.Func = Props.Func
-            Obj.DoubleClick = Props.DoubleClick
-            Obj.Tooltip = Props.Tooltip
-            Obj.Half = Props.Half or false -- new setting
-        else
-            Obj.Text = select(1, ...)
-            Obj.Func = select(2, ...)
-            Obj.Half = false
-        end
-        assert(type(Obj.Func) == 'function', 'AddButton: `Func` callback is missing.')
-    end
-
-    ProcessButtonParams('Button', Button, ...)
-
-    local Groupbox = self
-    local Container = Groupbox.Container
-
-    local function CreateBaseButton(Button)
-        local widthScale = Button.Half and 0.5 or 1
-        local widthOffset = Button.Half and -2 or -4
-        if Buton.Half == true then
-                print('true')
+        -- TODO: Eventually redo this
+        local Button = {};
+        local function ProcessButtonParams(Class, Obj, ...)
+            local Props = select(1, ...)
+            if type(Props) == 'table' then
+                Obj.Text = Props.Text
+                Obj.Func = Props.Func
+                Obj.DoubleClick = Props.DoubleClick
+                Obj.Tooltip = Props.Tooltip
+            else
+                Obj.Text = select(1, ...)
+                Obj.Func = select(2, ...)
             end
-        local Outer = Library:Create('Frame', {
-            BackgroundColor3 = Color3.new(0, 0, 0);
-            BorderColor3 = Color3.new(0, 0, 0);
-            Size = UDim2.new(widthScale, widthOffset, 0, 20);
-            ZIndex = 5;
-        })
 
-        local Inner = Library:Create('Frame', {
-            BackgroundColor3 = Library.MainColor;
-            BorderColor3 = Library.OutlineColor;
-            BorderMode = Enum.BorderMode.Inset;
-            Size = UDim2.new(1, 0, 1, 0);
-            ZIndex = 6;
-            Parent = Outer;
-        })
+            assert(type(Obj.Func) == 'function', 'AddButton: `Func` callback is missing.');
+        end
 
-        local Label = Library:CreateLabel({
-            Size = UDim2.new(1, 0, 1, 0);
-            TextSize = 14;
-            Text = Button.Text;
-            ZIndex = 6;
-            Parent = Inner;
-        })
+        ProcessButtonParams('Button', Button, ...)
 
-        Library:Create('UIGradient', {
-            Color = ColorSequence.new({
-                ColorSequenceKeypoint.new(0, Color3.new(1, 1, 1)),
-                ColorSequenceKeypoint.new(1, Color3.fromRGB(212, 212, 212))
+        local Groupbox = self;
+        local Container = Groupbox.Container;
+
+        local function CreateBaseButton(Button)
+            local Outer = Library:Create('Frame', {
+                BackgroundColor3 = Color3.new(0, 0, 0);
+                BorderColor3 = Color3.new(0, 0, 0);
+                Size = UDim2.new(1, -4, 0, 20);
+                ZIndex = 5;
             });
-            Rotation = 90;
-            Parent = Inner;
-        })
 
-        Library:AddToRegistry(Outer, { BorderColor3 = 'Black' })
-        Library:AddToRegistry(Inner, { BackgroundColor3 = 'MainColor'; BorderColor3 = 'OutlineColor' })
+            local Inner = Library:Create('Frame', {
+                BackgroundColor3 = Library.MainColor;
+                BorderColor3 = Library.OutlineColor;
+                BorderMode = Enum.BorderMode.Inset;
+                Size = UDim2.new(1, 0, 1, 0);
+                ZIndex = 6;
+                Parent = Outer;
+            });
 
-        Library:OnHighlight(Outer, Outer,
-            { BorderColor3 = 'AccentColor' },
-            { BorderColor3 = 'Black' }
-        )
+            local Label = Library:CreateLabel({
+                Size = UDim2.new(1, 0, 1, 0);
+                TextSize = 14;
+                Text = Button.Text;
+                ZIndex = 6;
+                Parent = Inner;
+            });
 
-        return Outer, Inner, Label
-    end
+            Library:Create('UIGradient', {
+                Color = ColorSequence.new({
+                    ColorSequenceKeypoint.new(0, Color3.new(1, 1, 1)),
+                    ColorSequenceKeypoint.new(1, Color3.fromRGB(212, 212, 212))
+                });
+                Rotation = 90;
+                Parent = Inner;
+            });
 
-    local function InitEvents(Button)
-        local function WaitForEvent(event, timeout, validator)
-            local bindable = Instance.new('BindableEvent')
-            local connection = event:Once(function(...)
-                if type(validator) == 'function' and validator(...) then
-                    bindable:Fire(true)
-                else
+            Library:AddToRegistry(Outer, {
+                BorderColor3 = 'Black';
+            });
+
+            Library:AddToRegistry(Inner, {
+                BackgroundColor3 = 'MainColor';
+                BorderColor3 = 'OutlineColor';
+            });
+
+            Library:OnHighlight(Outer, Outer,
+                { BorderColor3 = 'AccentColor' },
+                { BorderColor3 = 'Black' }
+            );
+
+            return Outer, Inner, Label
+        end
+
+        local function InitEvents(Button)
+            local function WaitForEvent(event, timeout, validator)
+                local bindable = Instance.new('BindableEvent')
+                local connection = event:Once(function(...)
+
+                    if type(validator) == 'function' and validator(...) then
+                        bindable:Fire(true)
+                    else
+                        bindable:Fire(false)
+                    end
+                end)
+                task.delay(timeout, function()
+                    connection:disconnect()
                     bindable:Fire(false)
-                end
-            end)
-            task.delay(timeout, function()
-                connection:disconnect()
-                bindable:Fire(false)
-            end)
-            return bindable.Event:Wait()
-        end
-
-        local function ValidateClick(Input)
-            if Library:MouseIsOverOpenedFrame() then return false end
-            if Input.UserInputType ~= Enum.UserInputType.MouseButton1 then return false end
-            return true
-        end
-
-        Button.Outer.InputBegan:Connect(function(Input)
-            if not ValidateClick(Input) then return end
-            if Button.Locked then return end
-
-            if Button.DoubleClick then
-                Library:RemoveFromRegistry(Button.Label)
-                Library:AddToRegistry(Button.Label, { TextColor3 = 'AccentColor' })
-
-                Button.Label.TextColor3 = Library.AccentColor
-                Button.Label.Text = 'Are you sure?'
-                Button.Locked = true
-
-                local clicked = WaitForEvent(Button.Outer.InputBegan, 0.5, ValidateClick)
-
-                Library:RemoveFromRegistry(Button.Label)
-                Library:AddToRegistry(Button.Label, { TextColor3 = 'FontColor' })
-
-                Button.Label.TextColor3 = Library.FontColor
-                Button.Label.Text = Button.Text
-                task.defer(rawset, Button, 'Locked', false)
-
-                if clicked then
-                    Library:SafeCallback(Button.Func)
-                end
-                return
+                end)
+                return bindable.Event:Wait()
             end
 
-            Library:SafeCallback(Button.Func)
-        end)
-    end
+            local function ValidateClick(Input)
+                if Library:MouseIsOverOpenedFrame() then
+                    return false
+                end
 
-    Button.Outer, Button.Inner, Button.Label = CreateBaseButton(Button)
-    Button.Outer.Parent = Container
+                if Input.UserInputType ~= Enum.UserInputType.MouseButton1 then
+                    return false
+                end
 
-    InitEvents(Button)
+                return true
+            end
 
-    function Button:AddTooltip(tooltip)
-        if type(tooltip) == 'string' then
-            Library:AddToolTip(tooltip, self.Outer)
+            Button.Outer.InputBegan:Connect(function(Input)
+                if not ValidateClick(Input) then return end
+                if Button.Locked then return end
+
+                if Button.DoubleClick then
+                    Library:RemoveFromRegistry(Button.Label)
+                    Library:AddToRegistry(Button.Label, { TextColor3 = 'AccentColor' })
+
+                    Button.Label.TextColor3 = Library.AccentColor
+                    Button.Label.Text = 'Are you sure?'
+                    Button.Locked = true
+
+                    local clicked = WaitForEvent(Button.Outer.InputBegan, 0.5, ValidateClick)
+
+                    Library:RemoveFromRegistry(Button.Label)
+                    Library:AddToRegistry(Button.Label, { TextColor3 = 'FontColor' })
+
+                    Button.Label.TextColor3 = Library.FontColor
+                    Button.Label.Text = Button.Text
+                    task.defer(rawset, Button, 'Locked', false)
+
+                    if clicked then
+                        Library:SafeCallback(Button.Func)
+                    end
+
+                    return
+                end
+
+                Library:SafeCallback(Button.Func);
+            end)
         end
-        return self
-    end
 
-    function Button:AddButton(...)
-        local SubButton = {}
-        ProcessButtonParams('SubButton', SubButton, ...)
+        Button.Outer, Button.Inner, Button.Label = CreateBaseButton(Button)
+        Button.Outer.Parent = Container
 
-        -- Force half size for main button when adding a sub-button
-        self.Outer.Size = UDim2.new(0.5, -2, 0, 20)
+        InitEvents(Button)
 
-        SubButton.Outer, SubButton.Inner, SubButton.Label = CreateBaseButton(SubButton)
-        SubButton.Outer.Position = UDim2.new(1, 3, 0, 0)
-        SubButton.Outer.Size = UDim2.fromOffset(self.Outer.AbsoluteSize.X - 2, self.Outer.AbsoluteSize.Y)
-        SubButton.Outer.Parent = self.Outer
-
-        function SubButton:AddTooltip(tooltip)
+        function Button:AddTooltip(tooltip)
             if type(tooltip) == 'string' then
                 Library:AddToolTip(tooltip, self.Outer)
             end
+            return self
+        end
+
+
+        function Button:AddButton(...)
+            local SubButton = {}
+
+            ProcessButtonParams('SubButton', SubButton, ...)
+
+            self.Outer.Size = UDim2.new(0.5, -2, 0, 20)
+
+            SubButton.Outer, SubButton.Inner, SubButton.Label = CreateBaseButton(SubButton)
+
+            SubButton.Outer.Position = UDim2.new(1, 3, 0, 0)
+            SubButton.Outer.Size = UDim2.fromOffset(self.Outer.AbsoluteSize.X - 2, self.Outer.AbsoluteSize.Y)
+            SubButton.Outer.Parent = self.Outer
+
+            function SubButton:AddTooltip(tooltip)
+                if type(tooltip) == 'string' then
+                    Library:AddToolTip(tooltip, self.Outer)
+                end
+                return SubButton
+            end
+
+            if type(SubButton.Tooltip) == 'string' then
+                SubButton:AddTooltip(SubButton.Tooltip)
+            end
+
+            InitEvents(SubButton)
             return SubButton
         end
 
-        if type(SubButton.Tooltip) == 'string' then
-            SubButton:AddTooltip(SubButton.Tooltip)
+        if type(Button.Tooltip) == 'string' then
+            Button:AddTooltip(Button.Tooltip)
         end
 
-        InitEvents(SubButton)
-        return SubButton
-    end
+        Groupbox:AddBlank(5);
+        Groupbox:Resize();
 
-    if type(Button.Tooltip) == 'string' then
-        Button:AddTooltip(Button.Tooltip)
-    end
-
-    Groupbox:AddBlank(5)
-    Groupbox:Resize()
-
-    return Button
-end
+        return Button;
+    end;
 
 
     function Funcs:AddDivider()
